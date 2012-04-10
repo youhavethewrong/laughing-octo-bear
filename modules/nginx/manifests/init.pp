8e98e648ab5930d8caef4df8eb5115145d99ee4b
negated <negated@ecik.youhavethewrong.info>
2012-04-10 13:48:26 -0500

    added custom fact for standard memory size. updated apache. added nginx.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,6 +1,37 @@
 # ESC 2012.04.09
-# NGINX manifest
+# NGINX module
+#------------------------------------------------------------------------------- 
 
 class nginx {
-    
+
+    # service depends on package and is notified when it changes
+    Package['nginx'] ~> Service['nginx']
+
+    # for some hosts, we want to use a slimmer version of nginx
+    if $memorysize_kb < 1024*1024 {
+        $which_pkg = 'nginx-lite',
+    } else {
+        $which_pkg = 'nginx-full',
+    }
+
+    # choose the version of nginx to install based on memory size
+    $pkg_name = $operatingsystem ? {
+        'Debian' => $which_pkg,
+        'Ubuntu' => $which_pkg,
+        'RedHat' => 'nginx',
+        'CentOS' => 'nginx',
+        default  => 
+          fail("OS ${operatingsystem} not supported by ${module_name} module."),
+    }
+
+    package { 'nginx':
+        ensure => latest,
+        name   => $pkg_name,
+    }
+
+    service { 'nginx':
+        ensure    => running,
+        hasstatus => true,
+    }
+
 }
