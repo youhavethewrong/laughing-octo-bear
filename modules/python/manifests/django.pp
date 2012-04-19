b87ebbc98beda907811ad5a2e680a3f48a546c2c
negated <negated@ecik.youhavethewrong.info>
2012-04-19 15:43:14 -0500

    added module for python-django

---
+++ modules/python/manifests/django.pp
@@ -0,0 +1,16 @@
+# ESC 2012.04.19
+# module for python-django, YAPWF
+
+class python::django {
+
+    $pkg_name = $operatingsystem ? {
+        'Debian' => 'python-django',
+        'Ubuntu' => 'python-django',
+        default  => fail("OS ${operatingsystem} not supported by ${module_name} module."),
+    }
+
+    package { 'python::pylons':
+        ensure => latest,
+        name   => $pkg_name,
+    }
+}
