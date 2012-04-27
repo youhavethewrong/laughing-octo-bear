9f51bf6a25c6c82ee0a67100374681e9cc202f81
negated <negated@ecik.youhavethewrong.info>
2012-04-27 10:43:21 -0500

    breaking up command for puppet parser

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -30,6 +30,6 @@ class java {
 
     exec { 'sun-java6-jdk-license-accept':
         command => "/bin/echo 'sun-java6-jdk shared/accepted-sun-dlj-v1-1 boolean true'|/usr/bin/debconf-set-selections",
-        unless  => "/usr/bin/debconf-get-selections|grep sun-java6-jdk|grep accepted-sun-dlj",
+        unless  => "/usr/bin/debconf-get-selections |grep sun-java6-jdk|grep accepted-sun-dlj",
     }
 }
