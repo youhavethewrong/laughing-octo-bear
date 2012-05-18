9775c4e3023536fbbc1de714ce40c8cbce5d9288
negated <negated@ecik.youhavethewrong.info>
2012-05-17 19:09:30 -0500

    fixing android

--- modules/android/manifests/init.pp
+++ modules/android/manifests/init.pp
@@ -15,15 +15,15 @@ class android {
     }
 
     exec { 'expand-android-sdk_r18':
-        command => "/bin/tar xzvf /usr/local/android-sdk_r18-linux.tgz -C /usr/local --owner negated --no-same-permissions",
+        command => "/bin/tar xzvf /usr/local/android-sdk_r18-linux.tgz -C /usr/local --no-same-owner --no-same-permissions && chgrp -R negated /usr/local/android-sdk-linux",
         unless  => "/usr/bin/test -d /usr/local/android-sdk-linux",
         require => File['/usr/local/android-sdk_r18-linux.tgz'],
     }
 
-#    exec { 'update-android-sdk_r18':
-#        command => "/usr/local/android-sdk-linux/tools/android update",
-#        unless  => "/usr/bin/test -d /usr/local/android-sdk-linux",
-#        require => File['/usr/local/android-sdk_r18-linux.tgz'],
-#    }
+    exec { 'update-android-sdk_r18':
+        command => "/usr/local/android-sdk-linux/tools/android update",
+        unless  => "/usr/bin/test -d /usr/local/android-sdk-linux/platforms/android-10",
+        require => File['/usr/local/android-sdk_r18-linux.tgz'],
+    }
  
 }
